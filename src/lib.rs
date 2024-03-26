2785a226bca6e0476d798b11896c01d7d6e84ffa
jredwards <jamesredwards@users.noreply.github.com>
2024-03-26 11:38:13 +0000

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,5 @@ pub mod tilde_expansion;
 mod tests {
     use crate::tilde_expansion::ExpandTilde;
 
-    #[test]
-    fn it_works() {
-        assert_eq!(
-            "~/Documents/oxygene lessons booking.pdf"
-                .expand_tilde()
-                .unwrap(),
-            std::path::PathBuf::from("/home/jedwards/Documents/oxygene lessons booking.pdf")
-        );
-    }
+
 }
